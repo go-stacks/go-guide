22968613c4b319660aefff40d312537472bf1992
442168402@qq.com <442168402@qq.com>
2023-02-12 19:54:08 +0800

    feat:  add once example

---
+++ internal/basic/sync/once_example.go
@@ -0,0 +1,18 @@
+package sync
+
+import (
+	"fmt"
+	"sync"
+)
+
+type OnceClose struct {
+	once sync.Once
+}
+
+func (o *OnceClose) Close() error {
+	o.once.Do(func() {
+		fmt.Println("close")
+	})
+
+	return nil
+}
---
+++ internal/basic/sync/once_example_test.go
@@ -0,0 +1,11 @@
+package sync
+
+import "testing"
+
+func TestOnce(t *testing.T) {
+	cc := &OnceClose{}
+
+	for i := 0; i < 100; i++ {
+		cc.Close()
+	}
+}
